1a5e80cbbbe693e1fbce05e5cc74ae3a1cfa730c
mfclem <michel.f.clement@gmail.com>
2026-07-28 01:09:13 +0200

    update Main.js

--- code/Main.js
+++ code/Main.js
@@ -12,7 +12,6 @@ function getThermostatsStates() {
 function run() {
   //authorizeTado();
   Logger = BetterLog.useSpreadsheet("1SoATMEhFWrdvawUuJr9K64U_L5fCpkNYa-zt1Ay5_4Q");
-  
   Logger.log("approved!");
   Logger.log("read rooms...");
   readRooms();
